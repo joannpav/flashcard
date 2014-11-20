db4be0012a31b66b154e12eaf9b4758b936150f5
Niki Macomber <nikicrisler@gmail.com>
2014-11-19 16:10:42 -0800

    deploy

--- flashgrabber/wsgi.py
+++ flashgrabber/wsgi.py
@@ -11,5 +11,5 @@ from dj_static import Cling
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stravaapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flashgrabber.settings")
 application = Cling(get_wsgi_application())
