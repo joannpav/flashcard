35efe3cc3bc4225b2a49020d698aac93f4cb9f73
Niki Macomber <nikicrisler@gmail.com>
2014-11-19 21:21:01 -0800

    hi

--- pytesser/__init__.py
+++ pytesser/__init__.py
@@ -10,7 +10,7 @@ import util
 import errors
 
 #tesseract_exe_name = '/usr/local/Cellar/tesseract/3.02.02_2/bin/tesseract' # Name of executable to be called at command line
-tesseract_exe_name = os.path.dirname(__file__) + '/tesseract' # Name of executable to be called at command line
+tesseract_exe_name = 'tesseract' # Name of executable to be called at command line
 scratch_image_name = "temp.bmp" # This file must be .bmp or other Tesseract-compatible format
 scratch_text_name_root = "temp" # Leave out the .txt extension
 cleanup_scratch_flag = True  # Temporary files cleaned up after OCR operation
